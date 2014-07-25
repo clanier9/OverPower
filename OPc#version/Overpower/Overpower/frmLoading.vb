1081fcdf9486b8bcb86875c5db3f4114b57cff0e
Cody <Cody@BadMotherDesktop.att.net>
2014-07-25 09:15:29 -0700

    removed old visual studio version and put it in the 'Deprecated' repo

--- OPc#version/Overpower/Overpower/frmLoading.vb
+++
@@ -1,8 +0,0 @@
-﻿
-Public Class frmLoading
-
-    Private Sub frmLoading_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        SplitContainer1.SplitterDistance = 0
-    End Sub
-
-End Class
